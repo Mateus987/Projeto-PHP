6299315b4f73669799136bbdd24627e92a5d9a16
Mateus dos Santos de Andrade <63256486+Mateus987@users.noreply.github.com>
2023-02-01 13:49:54 -0500

    Add files via upload

---
+++ teste.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projeto PHP</title>
+    <link rel="icon" href="https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg"/>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	
+<?php
+	$teste = "Olá Mundo";
+	ECHO "{$teste}<br>";
+	echo "Hello World!<br>";
+	EcHo "Hello World!<br>";
+?>
+
+</body>
+</html>
